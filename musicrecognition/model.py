bfcf3543e042977c07ae1612b2df4c912b9ad3c1
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-08-30 00:24:14 +0200

    Added first layout of the model.

--- musicrecognition/model.py
+++ musicrecognition/model.py
@@ -1,7 +1,49 @@
 import torch
 from torch import nn
+from torch.autograd import Variable
 
 
-model = nn.Sequential([
-    nn.Conv2d
-])
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.cnn_layers = nn.Sequential(
+            # Defining a
+        )
+
+    def forward(self):
+        pass
+
+
+class LSTM(nn.Module):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+        super(LSTM, self).__init__()
+        self.num_classes = num_classes  # number of classes
+        self.num_layers = num_layers  # number of layers
+        self.input_size = input_size  # input size
+        self.hidden_size = hidden_size  # hidden state
+        self.seq_length = seq_length  # sequence length
+
+        self.lstm = nn.LSTM(
+            input_size=input_size, hidden_size=hidden_size,
+            num_layers=num_layers, batch_first=True
+        )
+        # self.fc_1 =  nn.Linear(hidden_size, 128) #fully connected 1
+
+        # self.fully_connected = nn.Linear(128, num_classes)  # fully connected last layer
+
+    def forward(self, x):
+        # hidden state
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        # internal state
+        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        # Propagate input through LSTM
+        # lstm with input, hidden, and internal state
+        output, (hn, cn) = self.lstm(x, (h_0, c_0))
+        # reshaping the data for Dense layer next
+        hn = hn.view(-1, self.hidden_size)
+        out = self.relu(hn)
+        out = self.fc_1(out)
+        out = self.relu(out)
+        out = self.fc(out)
+        return out
